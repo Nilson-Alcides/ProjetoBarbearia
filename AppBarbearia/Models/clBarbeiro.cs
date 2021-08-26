b83ccf81490fa168341a5d375766c733e0ec0ae3
Nilson Alcides <nilson_alcides@hotmail.com.com>
2021-08-26 20:24:57 -0300

    Classe cliente e barbeiro

---
+++ AppBarbearia/Models/clBarbeiro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppBarbearia.Models
+{
+    public class clBarbeiro
+    {
+        public int codBarbeiro { get; set; }
+        public string nomeBarbeiro { get; set; }
+    }
+}
---
+++ AppBarbearia/Models/clCliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppBarbearia.Models
+{
+    public class clCliente
+    {
+        public int codCli { get; set; }
+        public string nomeCLi { get; set; }
+        public string telefoneCli { get; set; }
+        public string celularCli { get; set; }
+        public string EmailCli { get; set; }
+    }
+}
+
+ 
+ 
